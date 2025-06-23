5be17e986506b2a222eeb7e3624ae801e5e12450
Nitinsai <nitinsai.avirneni@gmail.com>
2025-06-23 14:35:22 +0530

    header and leftside working

---
+++ force-app/main/default/lwc/leftBody/leftBody.js
@@ -0,0 +1,36 @@
+import { LightningElement } from 'lwc';
+
+export default class LeftBody extends LightningElement {
+    stores = [
+        {
+            id: 1,
+            name: 'Apple Store Downtown',
+            reviews: 342,
+            followers: 15200,
+            posts: 156,
+            tweets: 234,
+            highlight: true,
+            status: ['green', 'green', 'green', 'green']
+        },
+        {
+            id: 2,
+            name: 'Apple Store Westfield Mall',
+            reviews: 198,
+            followers: 9800,
+            posts: 89,
+            tweets: 0,
+            highlight: false,
+            status: ['green', 'red', 'red', 'green']
+        },
+        {
+            id: 3,
+            name: 'Apple Store Northside',
+            reviews: 267,
+            followers: 12100,
+            posts: 134,
+            tweets: 189,
+            highlight: false,
+            status: ['green', 'green', 'green', 'green']
+        }
+    ];
+}
---
+++ force-app/main/default/lwc/socialMediaSite/socialMediaSite.js
@@ -0,0 +1,18 @@
+import { LightningElement } from 'lwc';
+
+export default class SocialMediaSite extends LightningElement {
+
+    testAnalytics() {
+        console.log('Social Media Site Test Analytics');
+    }
+
+// this is a test function to check the analytics of the social media site
+
+
+testSchedule(){
+    console.log('Social Media Site Test Schedule');
+}
+
+// this is a test function to check the schedule of the social media site
+
+}
